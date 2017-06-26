83e946a904330611ceb03a3134e0d5266aeac841
Logan Sease <lsease@gmail.com>
2017-06-26 10:47:09 -0400

    initial commit

---
+++ BlockStackBrowser/ImageCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCollectionViewCell.swift
+//  Bedkin
+//
+//  Created by lsease on 10/4/16.
+//  Copyright © 2016 Bedkin. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+}
