06175abd57e255ddf0eb5a45b2d86177a347f2ca
Logan Sease <lsease@gmail.com>
2017-06-29 11:56:41 -0400

    updated profile search to use onename matching the browser web app

--- BlockStackBrowser/ViewControllers/ProfileSearchController.swift
+++ BlockStackBrowser/ViewControllers/ProfileSearchController.swift
@@ -13,7 +13,7 @@ class ProfileSearchController: UIViewController {
     
     @IBOutlet var tableView : UITableView!
     @IBOutlet var searchText : UITextField!
-    var results : [BSSearchResponse.BSSearchResult] = []
+    var results : [BSSearchResponse.BSOneNameSearchResult] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -61,7 +61,7 @@ extension ProfileSearchController : UITableViewDelegate, UITableViewDataSource
         // Configure the cell...
         if indexPath.row < results.count
         {
-            cell.textLabel?.text = results[indexPath.row].profile.description
+            cell.textLabel?.text = results[indexPath.row].username ?? "?"
         }else{
             if let query = searchText.text, query.count > 0{
                 cell.textLabel?.text = "No Results Found For '\(query)'"
